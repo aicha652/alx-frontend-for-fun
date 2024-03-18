39444c78e22053645404c0cda2f3fee2faee3c3c
aicha652 <aichaazlf652@gmail.com>
2024-03-18 13:41:37 +0000

    fix pycodestyle

--- markdown2html.py
+++ markdown2html.py
@@ -8,9 +8,10 @@ import os.path
 
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        print('Usage: ./markdown2html.py '
+              'README.md README.html', file=sys.stderr)
         exit(1)
-    elif (os.path.isfile(sys.argv[1]) == False):
+    elif (os.path.isfile(sys.argv[1]) is False):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
     else:
